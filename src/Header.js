afaf72fb450927ac9aacfcb18438efcd40b998dd
ilyamed <0ilya@bk.ru>
2021-02-26 15:03:09 +0300

    init and add header skelet

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="header">
+      <header className="header">
+        <div className="header-logo"></div>
+        <h2>Дневник</h2>
+        <input typ="text"></input>
+        <div className="header-smile"></div>
+        <button className="header-button-list"></button>
+        <button className="header-button-add"></button>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
