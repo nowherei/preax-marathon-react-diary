13f07fbc0c036cfbeff139de199958d1a9a325fb
paradoxalyty <paradoxalyty@gmail.com>
2021-03-01 18:52:42 +0100

    some little bugs fixed

--- src/MobGallery.js
+++ src/MobGallery.js
@@ -3,7 +3,7 @@ import { createClient } from 'pexels';
 const client = createClient('563492ad6f917000010000012a3d4ec509bc4ffb9c82eec3eae30746');
 
 function MobGallery() {
-    client.photos.curated({ per_page: 30 }).then(photos => {
+    client.photos.curated({ per_page: 3 }).then(photos => {
         for (let i = 0; i < (photos.photos.length); i++) {
             let img = document.createElement('img');
             (i % 2 === 0) ? img.src = photos.photos[i].src.landscape : img.src = photos.photos[i].src.portrait;
